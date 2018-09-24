cbe8e17052b02db06019d9445dc43b366f33ba59
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2018-09-24 12:18:55 +0100

    :white_check_mark: Add first test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../components/App';
 
 it('renders without crashing', () => {
+  const location = { pathname: '/' };
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  
+  ReactDOM.render(<App location={location} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
