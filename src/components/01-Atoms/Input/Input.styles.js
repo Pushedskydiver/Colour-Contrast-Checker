93514535a17ebe705e3f9b723738ab50eb2cb4a7
Alex Clapperton <Alex.Clapperton@codecomputerlove.com>
2018-09-27 12:23:26 +0100

    :lipstick: Update input styles

--- src/components/01-Atoms/Input/Input.styles.js
+++ src/components/01-Atoms/Input/Input.styles.js
@@ -4,15 +4,10 @@ import { minWidth } from '../../../styles/settings.breakpoints.styles';
 
 const InputStyles = styled.input`
   width: 100%;
-  border-bottom: 2px solid transparent;
+  border-bottom: 2px solid currentColor;
   color: inherit;
   font-size: ${typography.heading.size.big};
   transition: border-bottom-color 0.3s ease-in-out;
-
-  &:active,
-  &:focus {
-    border-bottom-color: currentColor;
-  }
 
   ${minWidth('768', () => css`
     font-size: ${typography.heading.size.large};
