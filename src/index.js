7efb8e8cab9a2a1cda2080ce1ce9aa410620dd43
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2018-12-08 20:38:21 +0000

    :sparkles: Add prerendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import Router from './components/Router';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<Router />, document.querySelector('#main'));
+const rootElement = document.querySelector('#main');
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<Router />, rootElement);
+} else {
+  render(<Router />, rootElement);
+}
 registerServiceWorker();
