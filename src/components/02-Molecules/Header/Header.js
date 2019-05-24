e7a5786304ce08bfe03a2dfdee0dccb54f072aa3
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2019-05-24 19:54:02 +0100

    :fire: Remove chrome extension install

--- src/components/02-Molecules/Header/Header.js
+++ src/components/02-Molecules/Header/Header.js
@@ -3,16 +3,6 @@ import { Heading1 } from '../../01-Atoms/Heading/Heading.styles';
 import SkipLink from '../../01-Atoms/SkipLink/SkipLink.styles';
 import HeaderStyles from './Header.styles';
 
-const chromeExtension = () => {
-  if (window.chrome) {
-    if (window.chrome.app.isInstalled) {
-      return console.log('User has installed the app');
-    }
-
-    return console.log('User has not installed the app');
-  }
-};
-
 const Header = props => (
   <HeaderStyles>
     <SkipLink href="#ratio" color={props.colorState}>Skip to colour contrast ratio</SkipLink>
@@ -23,8 +13,6 @@ const Header = props => (
     <SkipLink href="#normalCopy" color={props.colorState}>Skip to normal text example copy</SkipLink>
 
     <Heading1 medium noMargin>Colour Contrast Checker</Heading1>
-
-    {chromeExtension()}
   </HeaderStyles>
 );
 
