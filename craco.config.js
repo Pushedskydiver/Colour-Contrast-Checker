9919ce2be8a9db801850921b7a4f205ef2eab8f5
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-06-26 11:42:35 +0100

    style: CC-001 - 💄 Update buy me coffee cta styles

--- craco.config.js
+++ craco.config.js
@@ -1,26 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('postcss-preset-env')({
-          stage: 0,
-          features: {
-            'custom-media-queries': true,
-            'custom-properties': false,
-            'nesting-rules': true,
-          },
-        }),
-        require('postcss-nested'),
-        require('postcss-sort-media-queries')({
-          sort: function (a, b) {
-            return b - a;
-          },
-        }),
-      ],
-    },
-  },
   webpack: {
     alias: {
       '~': path.resolve(__dirname, 'src')
