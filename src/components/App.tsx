d0b2d724d1229947e6a54876ef4fdb90bb6978cd
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-06-27 17:15:45 +0100

    fix: CC-001 - 🐛 Update ts config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
 import { Suspense, lazy } from 'react';
 import ColourContrastProvider from './Context';
 
-import { Divider } from '~/components/01-Atoms/divider/divider';
-import { Actions } from '~/components/02-Molecules/actions/actions';
-import { Footer } from '~/components/02-Molecules/footer/footer';
-import { Header } from '~/components/02-Molecules/header/header';
-import { ColorControls } from '~/components/03-Organisms/color-controls/color-controls';
-import { CopyExamples } from '~/components/03-Organisms/copy-examples/copy-examples';
-import { Score } from '~/components/03-Organisms/score/score';
+import { Divider } from './01-Atoms/divider/divider';
+import { Actions } from './02-Molecules/actions/actions';
+import { Footer } from './02-Molecules/footer/footer';
+import { Header } from './02-Molecules/header/header';
+import { ColorControls } from './03-Organisms/color-controls/color-controls';
+import { CopyExamples } from './03-Organisms/copy-examples/copy-examples';
+import { Score } from './03-Organisms/score/score';
 
 import '../styles/globals.css';
 
