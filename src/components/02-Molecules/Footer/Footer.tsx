733b930fe9a19b4c25b2eeee07d328b2dbe2a23e
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-06-27 16:37:42 +0100

    chore: CC-001 - Update footer and icons

--- src/components/02-Molecules/Footer/Footer.tsx
+++ src/components/02-Molecules/Footer/Footer.tsx
@@ -1,51 +1,41 @@
-import clsx from 'clsx';
-import { useColourContrast } from '~/components/Context';
-import { isDark } from '~/components/Utils';
 import { GitHub, Twitter } from '~/components/01-Atoms/Icon/Icon';
 import { Text } from '~/components/01-Atoms/text/text';
 
 import styles from './footer.module.css';
 
-export const Footer: React.FC = () => {
-  const { background, contrast } = useColourContrast();
-  const isPoorContrast = contrast < 3;
-  const isBackgroundDark = isDark(background);
+export const Footer: React.FC = () => (
+  <footer className={styles.footer}>
+    <div className={styles.container}>
+      <ul
+        aria-label="Social media links"
+        className={styles.list}
+      >
+        <li>
+          <a
+            href="https://github.com/Pushedskydiver/Colour-Contrast-Checker"
+            aria-label="GitHub project"
+          >
+            <GitHub />
+          </a>
+        </li>
 
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.container}>
-        <ul className={styles.list} aria-label="Social media links">
-          <li>
-            <a
-              href="https://github.com/Pushedskydiver/Colour-Contrast-Checker"
-              aria-label="GitHub project"
-            >
-              <GitHub />
-            </a>
-          </li>
+        <li>
+          <a
+            href="https://twitter.com/alexmclapperton"
+            title="Alex's Twitter profile"
+          >
+            <Twitter />
+          </a>
+        </li>
+      </ul>
 
-          <li>
-            <a
-              href="https://twitter.com/alexmclapperton"
-              title="Alex's Twitter profile"
-            >
-              <Twitter />
-            </a>
-          </li>
-        </ul>
-
-        <Text
-          tag="p"
-          weight="medium"
-          className={clsx(
-            styles.footnote,
-            isPoorContrast && !isBackgroundDark ? styles.footnoteDark : undefined,
-            isPoorContrast && isBackgroundDark ? styles.footnoteLight : undefined,
-          )}
-        >
-          UI created by <a href="https://www.willtarpey.com/" rel="external">Will Tarpey</a>. Thank you Will ❤️
-        </Text>
-      </div>
-    </footer>
-  );
-}
+      <Text
+        tag="p"
+        weight="medium"
+        className={styles.footnote}
+      >
+        UI created by <a href="https://www.willtarpey.com/" rel="external">Will Tarpey</a>. Thank you Will ❤️
+      </Text>
+    </div>
+  </footer>
+);
