2b3c1adade268aaea2e01b22ef40bd59dc7a2d25
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-07-01 13:40:35 +0100

    chore: CC-001 - 🎨 Update typeface and example

--- src/components/02-molecules/example/example.tsx
+++ src/components/02-molecules/example/example.tsx
@@ -15,16 +15,6 @@ export const Example: React.FC<TExample> = ({ id, labelText, size = 'normal' }) 
 
   const defaultText = 'Click/Tap to edit me. That Biff, what a character. Always trying to get away with something. Been on top of Biff ever since high school. Although, if it wasn\'t for him - Yes, yes, I\'m George, George McFly, and I\'m your density. I mean, I\'m your destiny. Right. Alright, take it up, go. Doc. Something wrong with the starter, so I hid it.';
 
-  const checkDataInput = (
-    e: React.FocusEvent<HTMLTextAreaElement, Element>
-  ): void => {
-    const target = e.target;
-
-    if (target.value.length === 0) {
-      target.value = defaultText;
-    }
-  }
-
   return (
     <div
       className={clsx(
@@ -45,7 +35,6 @@ export const Example: React.FC<TExample> = ({ id, labelText, size = 'normal' }) 
 
       <textarea
         id={id}
-        onBlur={checkDataInput}
         rows={5}
         className={clsx(
           styles.textarea,
