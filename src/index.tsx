c00f5f0fa815e91e6c990342b488d4e913283580
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-07-01 10:31:32 +0100

    chore: CC-001 - 🎨 Update service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import Router from './components/router';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.querySelector('#main');
 const root = createRoot(container!);
 
 root.render(<Router />);
 
-registerServiceWorker();
+serviceWorkerRegistration.register();
