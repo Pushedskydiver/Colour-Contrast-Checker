bdd461c32bce23f69607f62958ebe2580cdf5b23
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-07-01 11:29:21 +0100

    build: CC-001 - 🔧 Update craco config

--- craco.config.ts
+++ craco.config.ts
@@ -3,6 +3,11 @@ import path from 'path';
 import type { CracoConfig } from '@craco/types';
 
 const config: CracoConfig = {
+  babel: {
+    loaderOptions: {
+      exclude: [/node_modules\/@babel\/plugin-transform-classes/],
+    },
+  },
   style: {
     postcss: {
       mode: 'file',
